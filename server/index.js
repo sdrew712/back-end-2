c3e85fe0900d8f4f5e104e0bf7f5536a96def01b
Sarah Drew <drew.sarah.e@gmail.com>
2021-08-17 13:05:03 -0700

    finished assignmentt

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const ctrl = require("./controller")
+
+app.get("/api/houses", ctrl.getHouses)
+
+app.post("/api/houses", ctrl.createHouse)
+
+app.put("/api/houses/:id", ctrl.updateHouse)
+
+app.delete("/api/houses/:houseId", ctrl.deleteHouse)
+
+app.listen(4004, () => console.log("Take us to warp 4004!"));
